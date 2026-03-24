65db4e3d223faf503fb5686cef87a67168e89a1d
Texarkanine <texarkanine@protonmail.com>
2026-03-24 18:42:35 -0500

    fix: Found & included 'From Gravitons to Galaxies' book

--- lib/retrieve.rb
+++ lib/retrieve.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 require "fileutils"
+require_relative "wayback_patch"
 
 module Retrieve
-  SNAPSHOT_CUTOFF = "20180101000000"
+  SNAPSHOT_CUTOFF = "20180825193951"
 
   PAGES = [
     { url: "http://nuclearpyramid.com/",                           timestamp: "20170509195054" },
@@ -11,11 +12,8 @@ module Retrieve
     { url: "http://www.nuclearpyramid.com/other_two_pyramids.php", timestamp: "20170420190729" },
     { url: "http://nuclearpyramid.com/proton.php",                 timestamp: "20171010225935" },
     { url: "http://nuclearpyramid.com/greatpyramid/fig001.gif",    timestamp: "20170320154414" },
+    { url: "http://nuclearpyramid.com/From%20Gravitons%20to%20Galaxies.docx", timestamp: "20180825193950" }
   ].freeze
-
-  def self.download_cmd(page, dest_dir)
-    "wayback_machine_downloader #{page[:url]} -e --page-requisites -t #{page[:timestamp]} -d #{dest_dir}"
-  end
 
   def self.download_all(dest_dir)
     FileUtils.mkdir_p(dest_dir)
@@ -24,18 +22,16 @@ module Retrieve
         raise "BLOCKED: #{page[:url]} timestamp #{page[:timestamp]} is at or after SNAPSHOT_CUTOFF #{SNAPSHOT_CUTOFF}"
       end
       sleep(5) if i > 0
-      cmd = download_cmd(page, dest_dir)
-      puts "Running: #{cmd}"
-      system(cmd) || warn("Download failed for #{page[:url]}")
+      puts "Downloading #{page[:url]} @ #{page[:timestamp]}"
+      downloader = WaybackMachineDownloader.new(
+        base_url: page[:url],
+        exact_url: true,
+        page_requisites: true,
+        to_timestamp: page[:timestamp],
+        directory: dest_dir
+      )
+      downloader.download_files
     end
-    cleanup(dest_dir)
-  end
-
-  def self.cleanup(dest_dir)
-    html = File.join(dest_dir, "index.html")
-    php = File.join(dest_dir, "index.php")
-    File.rename(html, php) if File.exist?(html) && !File.exist?(php)
-
     relocate_encoded_dirs(dest_dir)
   end
 
